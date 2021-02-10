7d3b28c18355c58887a2a3318cb0e038470216f8
aver803bath5 <aver803bath261@gmail.com>
2021-02-10 10:32:25 +0800

    refactor(controller): return feature resource instead of feature domain model

--- Controllers/FeaturesController.cs
+++ Controllers/FeaturesController.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using VegaForCourse.Controllers.Resources;
 using VegaForCourse.Models;
 using VegaForCourse.Persistence;
 
@@ -10,17 +13,19 @@ namespace VegaForCourse.Controllers
     public class FeaturesController : Controller
     {
         private readonly VegaDbContext _context;
+        private readonly IMapper _mapper;
 
-        public FeaturesController(VegaDbContext context)
+        public FeaturesController(VegaDbContext context, IMapper _mapper)
         {
             _context = context;
+            this._mapper = _mapper;
         }
 
         [HttpGet("/api/features")]
-        public async Task<IEnumerable<Feature>> GetFeatures()
+        public async Task<IActionResult> GetFeatures()
         {
             var features = await _context.Features.ToListAsync();
-            return features;
+            return Ok(features.Select(_mapper.Map<Feature, FeatureResource>));
         }
     }
 }
