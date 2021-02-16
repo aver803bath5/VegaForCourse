868d0524cf02d221d5ec4397104a658a315d573d
aver803bath5 <aver803bath261@gmail.com>
2021-02-16 14:51:27 +0800

    remove unused name space

--- Startup.cs
+++ Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using VegaForCourse.Controllers.Resources;
 using VegaForCourse.Core;
 using VegaForCourse.Mapping;
 using VegaForCourse.Persistence;
