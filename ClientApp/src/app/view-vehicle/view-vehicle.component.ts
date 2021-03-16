f3bba1306de15aad902cdaa926c6b06f290ab04a
aver803bath5 <aver803bath261@gmail.com>
2021-03-16 10:27:00 +0800

    feat(view-vehicle-component): add upload input and show all images of the vehicle
    * I also add a cancel button to cancel the uploading process.

--- ClientApp/src/app/view-vehicle/view-vehicle.component.ts
+++ ClientApp/src/app/view-vehicle/view-vehicle.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { VehicleService } from "../services/vehicle.service";
 import { IVehicle } from "../core/models/IVechicle";
 import { ActivatedRoute, Router } from "@angular/router";
+import { PhotoService } from "../services/photo.service";
+import { HttpEventType, HttpResponse } from "@angular/common/http";
+import { finalize, takeUntil } from "rxjs/operators";
+import { fromEvent } from "rxjs";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
   selector: 'app-view-vehicle',
@@ -9,19 +14,29 @@ import { ActivatedRoute, Router } from "@angular/router";
   styleUrls: ['./view-vehicle.component.css']
 })
 export class ViewVehicleComponent implements OnInit {
+  @ViewChild('fileInput', { static: true }) fileInput: ElementRef;
+  @ViewChild('cancelButton', { static: false }) cancelButton: ElementRef;
   vehicleId = 0;
   vehicle: IVehicle = null;
+  photos = [];
+  progress: any = {
+    percentage: -1
+  }
 
   constructor(
     private route: ActivatedRoute,
     private vehicleService: VehicleService,
-    private router: Router
+    private router: Router,
+    private photoService: PhotoService,
+    private toastr: ToastrService
   ) {
   }
 
   ngOnInit() {
     this.vehicleId = +this.route.snapshot.paramMap.get('id');
     this.vehicleService.getVehicle(this.vehicleId).subscribe(v => this.vehicle = v);
+
+    this.photoService.getPhotos(this.vehicleId).subscribe(photos => this.photos = photos as any[]);
   }
 
   delete() {
@@ -31,5 +46,32 @@ export class ViewVehicleComponent implements OnInit {
           this.router.navigate(['/']);
         });
     }
+  }
+
+  uploadPhoto() {
+    const nativeElement: HTMLInputElement = this.fileInput.nativeElement;
+    if (nativeElement.files.length === 0) return;
+
+    // Get the file value and then reset the fileInput value.
+    const file = nativeElement.files[0];
+    nativeElement.value = '';
+    this.photoService.upload(this.vehicleId, file)
+      .pipe(
+        takeUntil(fromEvent(this.cancelButton.nativeElement as HTMLButtonElement, 'click')),
+        finalize(() => {
+          // Reset progress percentage
+          this.progress.percentage = -1;
+        })
+      )
+      .subscribe(event => {
+          if (event.type === HttpEventType.UploadProgress) {
+            this.progress.percentage = Math.round(100 * event.loaded / event.total);
+          } else if (event instanceof HttpResponse) {
+            this.photos.push(event.body)
+          }
+        },
+        err => {
+          this.toastr.error(err.error, 'Error');
+        });
   }
 }
